db761fffbf4479a9296aeb344efe11182a967e9d
Akash Badshah <akashbad@mit.edu>
2012-10-20 13:08:44 -0400

    Created the rails app and a homepage

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FilepickerDashboard::Application.config.secret_token = '03884adce7baeb0dddafab0abaec1c2432df3701c7f93d245d2087fd5ff937acb13a64863152a79d602300203e04f7e37cb5566bde6c088c6a53bf93a489fe49'
